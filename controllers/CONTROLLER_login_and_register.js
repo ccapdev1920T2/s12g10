32438805a9736a126aeead7ee374df326ec13df9
geosefuy <geosef_viktor_uy@dlsu.edu.ph>
2020-04-02 19:19:25 +0800

    Register controller

--- controllers/CONTROLLER_login_and_register.js
+++ controllers/CONTROLLER_login_and_register.js
@@ -17,6 +17,26 @@ const controller = {
             }
         });
         res.render("pages/login_and_register")
+    },
+    authenticateUser: function(req, res){
+        
+    },
+    addUser: function(req, res){
+        var fname = req.body.fname;
+        var lname = req.body.lname;
+        var bday = req.body.bday;
+        var gender = req.body.gender;
+        var email = req.body.email;
+        var pass = req.body.pass;
+        db.insertOne(User, {
+            fname: fname,
+            lname: lname,
+            bday: bday,
+            gender: gender,
+            email: email,
+            pass: pass
+        });
+        res.render('../views/pages/homepage?fname=' + fname + '&lname=' + lname);
     }
 };
 
