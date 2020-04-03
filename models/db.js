d72da334f10e6a5ec595f45e78ca45b8f89f6491
Byron <byron.nill10@gmail.com>
2020-04-03 18:41:35 +0800

    useCreateIndex: true

--- models/db.js
+++ models/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connection = "mongodb://localhost:27017/mnemosis";
 const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 };
 
 const db = {
