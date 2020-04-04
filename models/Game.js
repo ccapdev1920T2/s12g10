380b27deccf8e1ac203f28350d07a304378a9f94
geosefuy <geosef_viktor_uy@dlsu.edu.ph>
2020-04-04 17:46:22 +0800

    Create game updates

--- models/Game.js
+++ models/Game.js
@@ -4,43 +4,37 @@ const Item = require("./Item");
 const Attempt = require("./Attempt");
 
 const schema = {
-    genres: [String],
-    title: String,
-    description: String,
-    image: String,
-    creator: String
-
-    // title: {
-    //     type: String,
-    //     required: true,
-    //     minlength: 10,
-    //     maxlength: 50
-    // },
-    // description: {
-    //     type: String,
-    //     required: true,
-    //     minlength: 10,
-    //     maxlength: 500
-    // },
-    // game_image: {
-    //     type: String,
-    //     required: false
-    // },
-    // genres: {
-    //     type: [String],
-    //     required: true,
-    //     enum: ["art", "business", "scitech", "general", "trivia", "sports", "others"]
-    // },
-    // time: {
-    //     type: Number,
-    //     required: true,
-    //     min: 3,
-    //     max: 10
-    // },
-    // creator: {
-    //     type: User,
-    //     required: true
-    // }
+    title: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 50
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 500
+    },
+    game_image: {
+        type: String,
+        required: false
+    },
+    genres: {
+        type: [String],
+        required: true,
+        enum: ["art", "business", "scitech", "general", "trivia", "sports", "others"]
+    },
+    time: {
+        type: Number,
+        required: true,
+        min: 3,
+        max: 10
+    },
+    creator: {
+        type: mongoose.ObjectId,
+        required: true
+    }
 };
 const collection = "games";
 
