d39b04c13f870716cc514369263bed64072abf69
geosefuy <geosef_viktor_uy@dlsu.edu.ph>
2020-04-09 19:33:13 +0800

    multer removal

--- routes/ROUTE_create_game.js
+++ routes/ROUTE_create_game.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const multer = require("multer");
 const app = express();
-var upload = multer({ dest: 'media/game_images/' })
 
 const controller = require("../controllers/CONTROLLER_create_game");
 
@@ -9,6 +7,6 @@ app.get("/", function(req, res) {
     res.render("../views/pages/create_game", {guest: req.session.guest});
 });
 
-app.post("/create", upload.single('game_image'), controller.createGame);
+app.post("/create", controller.createGame);
 
 module.exports = app;
--- routes/ROUTE_modify_game_details.js
+++ routes/ROUTE_modify_game_details.js
@@ -3,8 +3,6 @@ const app = express();
 
 const controller = require("../controllers/CONTROLLER_modify_game_details");
 
-// app.use(fileUpload());
-
 app.get("/:id", controller.getGameDetails);
 
 app.post("/:id/modify", controller.modifyGame);
