66c34be7815fba3a722c57ef407c55883f25b094
Byron <byron.nill10@gmail.com>
2020-04-14 17:13:23 +0800

    imported string-similarity umd for play game

--- public/js/play_game.js
+++ public/js/play_game.js
@@ -43,47 +43,6 @@ function startCountdown (date, mins) {
 
 }
 
-function editDistance(s1, s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-
-    let costs = new Array();
-    for (let i = 0; i <= s1.length; i++) {
-        let lastValue = i;
-        for (let j = 0; j <= s2.length; j++) {
-            if (i == 0)
-                costs[j] = j;
-            else {
-                if (j > 0) {
-                    let newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue),
-                            costs[j]) + 1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
-            }
-        }
-        if (i > 0)
-            costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-}
-
-function similarity(s1, s2) {
-    let longer = s1;
-    let shorter = s2;
-    if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-    }
-    let longerLength = longer.length;
-    if (longerLength == 0) {
-        return 1.0;
-    }
-    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-}
-
 function endGame (status) {
 
     clearInterval(interval);
@@ -211,7 +170,7 @@ $("#answer").on("keyup", function (event) {
         let accepted;
 
         if (correct.length > 15) {
-            let sim = similarity(answer, correct);
+            let sim = stringSimilarity.compareTwoStrings(answer, correct);
             if (sim >= 0.75) {
                 accepted = true;
             } else {
